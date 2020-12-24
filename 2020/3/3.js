37cb8108062ea5f02fa5d4e6ff2a6a4e669fec5a
Byron Hinebaugh <466561+bhinebaugh@users.noreply.github.com>
2020-12-23 21:07:25 -0500

    rest of day 3

--- 2020/3/3.js
+++ 2020/3/3.js
@@ -1,37 +1,50 @@
 fs = require('fs');
 
 const treeChar = '#';
+const defaultSlope = {right: 3, down: 1};
+const slopeSet = [
+    {right: 1, down: 1},
+    {right: 3, down: 1},
+    {right: 5, down: 1},
+    {right: 7, down: 1},
+    {right: 1, down: 2},
+]
 
-function parseGrid(gridAsText) {
-    var grid = gridAsText.split('\n');
-    return grid;
-}
 function countTrees(gridAsText, slope) {
-    // parseGrid(grid)
     const grid = gridAsText.split('\n');
     const width = grid[0].length;
     var treeCount = 0;
     // start at 0,0
-    for (let row=1; row<grid.length; row++) {
-        const column = (slope*row) % width;
-        // if (column > row.length) break;
-        // column % grid.length
+    for (let i=0; (i*slope.down)<grid.length; i ++) {
+        const row = i * slope.down;
+        const column = (slope.right*i) % width;
         if (grid[row][column] === treeChar) { treeCount++ }
     }
     return treeCount;
 }
 
+function multiplyTreesOnAllSlopes(grid, slopeSet) {
+    const treesPerSlope = slopeSet.map(slope => countTrees(grid, slope));
+    return treesPerSlope.reduce((acc, cur) => acc*cur, 1)
+}
+
 function run() {
     fs.readFile('./input-3.txt', 'utf8', (err,data) => {
         if (err) { throw new Error(err) }
-        // initial slope: right 3, down 1
-        const result = countTrees(data, 3);
-        console.log('trees encountered:',result)
+
+        // single slope
+        const result1 = countTrees(data, defaultSlope);
+        console.log('trees encountered:',result1)
+
+        // multiple slopes
+        const result2 = multiplyTreesOnAllSlopes(data, slopeSet);
+        console.log('product of trees across set of slopes', result2)
     })
 }
 
 run();
 
 module.exports = {
-    countTrees
+    countTrees,
+    multiplyTreesOnAllSlopes
 }
--- 2020/3/3.test.js
+++ 2020/3/3.test.js
@@ -13,9 +13,26 @@ const testGrid = `..##.......
 #...##....#
 .#..#...#.#`;
 
-const testSlope = 3;
+const testSlope = {right: 3, down: 1};
+
+// Right 1, down 1.
+// Right 3, down 1. (This is the slope you already checked.)
+// Right 5, down 1.
+// Right 7, down 1.
+// Right 1, down 2.
+const slopeSet = [
+    {right: 1, down: 1, trees: 2},
+    {right: 3, down: 1, trees: 7},
+    {right: 5, down: 1, trees: 3},
+    {right: 7, down: 1, trees: 4},
+    {right: 1, down: 2, trees: 2},
+]
 
 test('number of trees encountered', t => {
     t.equals(m3.countTrees(testGrid, testSlope), 7);
+    slopeSet.forEach(slope => {
+        t.equals(m3.countTrees(testGrid, slope), slope.trees)
+    })
+    t.equals(m3.multiplyTreesOnAllSlopes(slopeSet), 336)
     t.end();
 })
